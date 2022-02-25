25ea9e1cd2e109171f69db3cdb95e32d611f865b
matthieu-ui <mattfelker206@outlook.com>
2022-02-24 16:58:21 -0800

    jbox modal

--- js/jquery.js
+++ js/jquery.js
@@ -1,26 +1,29 @@
-$("button").on("click", function(e) {
-    e.stopPropagation();
-    $("#menu").menu().toggle().position({
-        my: "left+45px top",
-        at: "top+25px",
-        of: $(this),
-        collision: "fit flip"
+$(document).ready(function() {
+
+
+    // Drag with title
+
+    new jBox('Modal', {
+        attach: '#desktopIcon',
+        width: 220,
+        title: 'Matthieu-UI Version 2022 v1.0',
+        overlay: false,
+        content: "I'm baby polaroid crucifix banjo pork belly. Hell of lyft gluten-free roof party woke street art austin PBR&B tbh prism pop-up cold-pressed. Normcore dreamcatcher pabst irony pug. DIY chillwave la croix, try-hard irony tbh put a bird on it snackwave.",
+        draggable: 'title',
+        repositionOnOpen: false,
+        repositionOnContent: false
     });
-});
 
-$(document).on("click", function(e) {
-    $("#menu").hide();
-});
+    new jBox('Modal', {
+        attach: '#taskbutton',
+        width: 220,
+        title: 'start',
+        overlay: false,
+        content: 'list links',
+        draggable: false,
+        repositionOnOpen: false,
+        repositionOnContent: false,
+        offset: { x: -200, y: 280 }
 
-/* ------------------------- draggable popup window ------------------------- */
-$(function() {
-    $("#draggable").draggable();
-});
-
-$(function() {
-    $(".css-draggable-box").draggable({
-        handle: ".css-draggable-title-bar",
-        cancel: ".css-close-button",
-        containment: "window"
     });
 });
